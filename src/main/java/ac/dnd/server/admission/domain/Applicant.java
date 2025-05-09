ae723266465482f930b6f1908d7f263e8d50d5e0
devksh930 <ksh930@icloud.com>
2025-05-10 04:02:16 +0900

    feat : Applicant 와 Event 연관관계 추가

--- src/main/java/ac/dnd/server/admission/domain/Applicant.java
+++ src/main/java/ac/dnd/server/admission/domain/Applicant.java
@@ -4,6 +4,11 @@ import ac.dnd.server.common.BaseEntity;
 import ac.dnd.server.enums.ApplicantStatus;
 import ac.dnd.server.enums.ApplicantType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,10 +19,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Applicant extends BaseEntity {
+
 	private String name;
+
 	private String email;
+
+	@Enumerated(EnumType.STRING)
 	private ApplicantType type;
+
+	@Enumerated(EnumType.STRING)
 	private ApplicantStatus status;
+
+	@ManyToOne(optional = false,
+		fetch = FetchType.LAZY)
+	@JoinColumn(name = "event_id")
+	private Event event;
 
 	public Applicant(
 		final String name,
@@ -30,4 +46,11 @@ public class Applicant extends BaseEntity {
 		this.type = type;
 		this.status = status;
 	}
+
+	public void withEvent(
+		final Event event
+	) {
+		this.event = event;
+	}
+
 }
