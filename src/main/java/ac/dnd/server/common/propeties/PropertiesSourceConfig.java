e02d40c29cc38675a523177fcbad4e25c43cd2b5
Devksh930 <45715241+devksh930@users.noreply.github.com>
2025-05-22 06:45:35 +0900

    feat : 조회시 복호화 로직 추가

---
+++ src/main/java/ac/dnd/server/common/propeties/PropertiesSourceConfig.java
@@ -0,0 +1,9 @@
+package ac.dnd.server.common.propeties;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({EncryptionTextProperties.class})
+public class PropertiesSourceConfig {
+}
