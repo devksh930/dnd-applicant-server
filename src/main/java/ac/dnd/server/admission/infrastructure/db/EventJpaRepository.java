3d0467e0938ef73190954128d1b83f6445ada0b7
Devksh930 <45715241+devksh930@users.noreply.github.com>
2025-05-22 06:46:25 +0900

    feat : 조회시 slack 메세지 전송을 위한 이벤트 발행 로직 추가

--- src/main/java/ac/dnd/server/admission/infrastructure/db/EventJpaRepository.java
+++ src/main/java/ac/dnd/server/admission/infrastructure/db/EventJpaRepository.java
@@ -1,4 +1,4 @@
-package ac.dnd.server.admission.infrastructure;
+package ac.dnd.server.admission.infrastructure.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/test/java/ac/dnd/server/admission/application/service/ApplicantQueryServiceTest.java
+++ src/test/java/ac/dnd/server/admission/application/service/ApplicantQueryServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import ac.dnd.server.admission.application.dto.ApplicantData;
@@ -18,7 +19,9 @@ import ac.dnd.server.admission.application.mapper.ApplicantMapper;
 import ac.dnd.server.admission.domain.AdmissionRepository;
 import ac.dnd.server.admission.domain.Applicant;
 import ac.dnd.server.admission.domain.ApplicantValidator;
+import ac.dnd.server.admission.domain.event.ApplicantQueryEvent;
 import ac.dnd.server.admission.exception.ApplicantNotFoundException;
+import ac.dnd.server.common.event.Events;
 import ac.dnd.server.fixture.ApplicantFixture;
 
 @ExtendWith(MockitoExtension.class)
@@ -87,4 +90,38 @@ class ApplicantQueryServiceTest {
 		);
 
 	}
+
+	@Test
+	void 합격자조회_이벤트가_정상적으로_발행된다() {
+		try (MockedStatic<Events> eventsMock = mockStatic(Events.class)) {
+			final Applicant fixture = ApplicantFixture.create();
+			when(admissionRepository.findAdmissionByEventIdAndNameAndEmail(
+				anyLong(),
+				anyString(),
+				anyString()
+			)).thenReturn(Optional.of(fixture));
+
+			when(applicantMapper.entityToData(
+				fixture
+			)).thenReturn(new ApplicantData(
+				fixture.getEvent().getName(),
+				fixture.getName(),
+				fixture.getEmail(),
+				fixture.getType(),
+				fixture.getStatus()
+			));
+
+			applicantQueryService.getApplicantStatusCheck(new ApplicantStatusCheckCommand(
+				1L,
+				"name",
+				"email"
+			));
+
+			eventsMock.verify(
+				() -> Events.raise(any(ApplicantQueryEvent.class)),
+				times(1)
+			);
+		}
+	}
+
 }
