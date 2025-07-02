98f51e5de07d01685faf5c82362a0564b314509d
Devksh930 <45715241+devksh930@users.noreply.github.com>
2025-07-02 11:20:53 +0900

    refactor : 패키지 구조 개선 및 enum 추가

--- src/main/kotlin/ac/dnd/server/admission/infrastructure/web/EventQueryController.kt
+++ src/main/kotlin/ac/dnd/server/admission/infrastructure/web/EventQueryController.kt
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/events")
@@ -22,7 +21,7 @@ class EventQueryController(
                 currentEvent.id,
                 currentEvent.name,
                 currentEvent.resultAnnouncementDateTime,
-                currentEvent.isAnnounceable(LocalDateTime.now())
+                currentEvent.isCompleted()
             )
         )
     }
