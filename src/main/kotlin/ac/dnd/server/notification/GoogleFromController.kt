8e437117eb53067be62bb2c9b1dc122eefe6f1d2
cadence9309 <ksh930@icloud.com>
2025-08-10 22:28:37 +0900

    feat : googleForm github issue로 등록

--- src/main/kotlin/ac/dnd/server/notification/GoogleFromController.kt
+++ src/main/kotlin/ac/dnd/server/notification/GoogleFromController.kt
@@ -1,22 +1,44 @@
 package ac.dnd.server.notification
 
-import org.springframework.web.bind.annotation.GetMapping
+import ac.dnd.server.notification.github.service.GitHubService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.Objects
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 @RestController
 @RequestMapping("/google")
-class GoogleFromController {
+class GoogleFromController(
+    private val gitHubService: GitHubService
+) {
 
-    @GetMapping("/form")
+    @PostMapping("/form")
     fun googleForm(
-        @RequestBody body: Map<String, Objects>
-    ): Map<String, Objects> {
-        println(body)
-        return body;
+        @RequestBody request: Map<String, String>,
+    ): ResponseEntity<Unit> {
 
+        val markdownText = StringBuilder()
+        request.forEach { (question, answer) ->
+            val title = question.split("\n").first().trim()
+            markdownText.append("### $title\n")
+            markdownText.append("$answer\n\n")
+        }
+
+        val markdownContent = markdownText.toString()
+        println(markdownContent)
+
+        // GitHub에 이슈 생성
+        val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+        val issueTitle = "코드리뷰 제출 - $timestamp"
+
+        val githubIssue = gitHubService.createIssue(issueTitle, markdownContent)
+
+
+        return ResponseEntity.ok().build();
     }
 }
