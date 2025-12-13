48aefd90c3ba24a1c6a3480a2abd3afe338b3f59
devksh930 <ksh930@icloud.com>
2025-12-13 22:45:42 +0900

    feat: ProjectRepository 인터페이스 수정

--- src/main/kotlin/ac/dnd/server/review/infrastructure/persistence/repository/FormLinkJpaRepository.kt
+++ src/main/kotlin/ac/dnd/server/review/infrastructure/persistence/repository/FormLinkJpaRepository.kt
@@ -2,5 +2,10 @@ package ac.dnd.server.review.infrastructure.persistence.repository
 
 import ac.dnd.server.review.infrastructure.persistence.entity.FormLink
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.UUID
 
-interface FormLinkJpaRepository : JpaRepository<FormLink, Long>
+interface FormLinkJpaRepository : JpaRepository<FormLink, Long> {
+
+    fun findByKey(key: UUID): FormLink?
+}
