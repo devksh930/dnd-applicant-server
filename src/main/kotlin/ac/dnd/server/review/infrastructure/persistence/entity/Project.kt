c01129f0469d5a8ee6c793721cba1d5471047220
devksh930 <ksh930@icloud.com>
2025-12-13 22:44:41 +0900

    feat: Project 관련 Entity 수정

--- src/main/kotlin/ac/dnd/server/review/infrastructure/persistence/entity/Project.kt
+++ src/main/kotlin/ac/dnd/server/review/infrastructure/persistence/entity/Project.kt
@@ -1,16 +1,21 @@
 package ac.dnd.server.review.infrastructure.persistence.entity
 
+import ac.dnd.server.review.domain.enums.SubmissionStatus
 import ac.dnd.server.review.domain.value.GenerationInfo
 import ac.dnd.server.shared.persistence.BaseEntity
 import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 
 @Entity
 class Project(
     @Embedded
     val generationInfo: GenerationInfo,
+    val name: String,
     val description: String,
     val techStack: String,
-
-    ) : BaseEntity() {
+    @Enumerated(EnumType.STRING)
+    val status: SubmissionStatus = SubmissionStatus.NONE
+) : BaseEntity() {
 }
--- src/main/kotlin/ac/dnd/server/review/infrastructure/persistence/entity/ProjectUrl.kt
+++ src/main/kotlin/ac/dnd/server/review/infrastructure/persistence/entity/ProjectUrl.kt
@@ -1,9 +1,7 @@
 package ac.dnd.server.review.infrastructure.persistence.entity
 
-import ac.dnd.server.review.domain.value.GenerationInfo
 import ac.dnd.server.review.domain.enums.UrlType
 import ac.dnd.server.shared.persistence.BaseEntity
-import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
@@ -14,9 +12,6 @@ class ProjectUrl(
 
     @ManyToOne(optional = false)
     val project: Project,
-
-    @Embedded
-    val generationInfo: GenerationInfo,
 
     @Enumerated(EnumType.STRING)
     val type: UrlType,
