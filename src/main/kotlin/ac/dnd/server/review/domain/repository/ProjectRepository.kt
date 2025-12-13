0d169bb53818c030402bfbd9ac6f1d4684814559
devksh930 <ksh930@icloud.com>
2025-12-13 22:45:42 +0900

    feat: ProjectRepository 인터페이스 수정

--- src/main/kotlin/ac/dnd/server/review/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/ac/dnd/server/review/domain/repository/ProjectRepository.kt
@@ -1,5 +1,10 @@
 package ac.dnd.server.review.domain.repository
 
-interface ProjectRepository {
+import ac.dnd.server.review.infrastructure.persistence.entity.Project
+import ac.dnd.server.review.infrastructure.persistence.entity.ProjectUrl
 
+interface ProjectRepository {
+    fun save(project: Project): Project
+    fun saveAllUrls(projectUrls: List<ProjectUrl>): List<ProjectUrl>
+    fun findProjectByLinkKey(linkKey: String) : Project?
 }
