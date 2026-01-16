af07afe3f0ccc36a2d231878fdcf1c04977cbf44
cadence9309 <ksh930@icloud.com>
2026-01-16 14:39:42 +0900

    refactor: domain repository에 entity-> domain model로 변경

--- src/main/kotlin/ac/dnd/server/review/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/ac/dnd/server/review/domain/repository/ProjectRepository.kt
@@ -1,14 +1,14 @@
 package ac.dnd.server.review.domain.repository
 
-import ac.dnd.server.review.infrastructure.persistence.entity.ProjectEntity
-import ac.dnd.server.review.infrastructure.persistence.entity.FormLinkEntity
-import ac.dnd.server.review.infrastructure.persistence.entity.ProjectUrlEntity
+import ac.dnd.server.review.domain.model.FormLink
+import ac.dnd.server.review.domain.model.Project
+import ac.dnd.server.review.domain.model.ProjectUrl
 
 interface ProjectRepository {
-    fun save(project: ProjectEntity): ProjectEntity
-    fun saveAllUrls(projectUrls: List<ProjectUrlEntity>): List<ProjectUrlEntity>
+    fun save(project: Project): Project
+    fun saveAllUrls(projectId: Long, projectUrls: List<ProjectUrl>)
     fun deleteUrlsByProjectId(projectId: Long)
-    fun findProjectByLinkKey(linkKey: String): ProjectEntity?
-    fun saveFormLink(formLink: FormLinkEntity): FormLinkEntity
-    fun findLinkByLinkKey(linkKey: String) :FormLinkEntity?
+    fun findProjectByLinkKey(linkKey: String): Project?
+    fun saveFormLink(formLink: FormLink): FormLink
+    fun findLinkByLinkKey(linkKey: String): FormLink?
 }
